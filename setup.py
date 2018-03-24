b553d66688b70dd196f9e3d60b6092b6eba3741e
JoshuaEnglish <Joshua.R.English@gmail.com>
2018-03-23 22:48:29 -0700

    THIS is the entry point

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='weather',
+    version='0.1',
+    py_modules=['cli'],
+    install_requires=['Click',],
+    entry_points={
+        'console_scripts': [
+            'weather = cli:main'
+            ]
+        }
+    )
